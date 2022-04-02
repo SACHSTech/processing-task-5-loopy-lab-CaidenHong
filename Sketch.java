e916efd49dbc49ae3e4c2e842b0a220677feac65
CaidenHong <75390871+CaidenHong@users.noreply.github.com>
2022-04-01 23:19:42 -0400

    completed

--- Sketch.java
+++ Sketch.java
@@ -1,83 +1,57 @@
 import processing.core.PApplet;
 
+  /**
+  * A program that generates 8 differnt patterns in 8 different quadrants.
+  * @author: C. Hong
+  */
+
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
     size(1200, 600);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(45, 150, 207);
+    background(93,138,168);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    /*
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-*/
-    draw_section_outlines();
-    draw_section1();
-    draw_section2();
-    draw_section3();
-    draw_section4();
-	  
-    draw_section5();
-    draw_section6();
-    draw_section7();
-    draw_section8();
-
-    
+    quadBorders();
+    quad1();
+    quad2();
+    quad3();
+    quad4();
+    quad5();
+    quad6();
+    quad7();
+    quad8();
   }
 
-
-  /**
-   * Draw the outlines for all sections
-   */
-  public void draw_section_outlines(){
+  public void quadBorders() {
     stroke(0);
     noFill();
 
-    // draw bottom row boxes
     rect(0,300, 300, 300);
     rect(300, 300, 300, 300);
     rect(600, 300, 300, 300);
     rect(900, 300, 300, 300);
 
-    // draw top row boxes
     rect(0,0, 300, 300);
     rect(300, 0, 300, 300);
     rect(600, 0, 300, 300);
     rect(900, 0, 300, 300);
   }
   
-  /**
-   * draws the bottom left section
-   */
-  public void draw_section1(){
+  // Plain grid
+  public void quad1(){
     int intX = 0;
     int intY = 0;
 
     for(int intRow = 0; intRow < 30; intRow++){
       for(int intColumn = 0; intColumn < 30; intColumn++){
-        intX = 3 + 0;  //Instead of zero, calculate the proper intX location using 'intRow'
-        intY = 300 + 3 + 0; //Instead of zero, calculate the proper intY location using 'intColumn'
+        intX = 3 + (intRow * 10);  
+        intY = 303 + (intColumn * 10); 
 
         fill(255);
         noStroke();
@@ -86,52 +60,164 @@ public class Sketch extends PApplet {
       }
     }
   }
+  
+  // Vertical grid
+  public void quad2(){
+    int intX = 0;
+    int intY = 0;
 
-  /**
-   * Use the modulus operator and an if statement to select the color
-   * Don't loop from 30 to 60 to shift everything over, just add 300 to x.
-   */
-  public void draw_section2(){
+    for(int intRow = 0; intRow < 30; intRow++){
+      for(int intColumn = 0; intColumn < 30; intColumn++){
+        intX = 303 + (intRow * 10);
+        intY = 303 + (intColumn * 10); 
 
+        fill(0, 0, 0);
+        noStroke();
+        rect(intX, intY, 5, 5);
+        
+        if(intRow % 2 == 0){
+          fill(255);
+          noStroke();
+          rect(intX, intY, 5, 5);
+          
+        }
+      }  
+    }
   }
 
-  /**
-   * Use the modulus operator and an if/else statement to select the color.
-   * Don't use multiple 'if' statements.
-   */
-  public void draw_section3(){
+  // Horizontal grid
+  public void quad3(){
+    int intX = 0;
+    int intY = 0;
 
+    for(int intRow = 0; intRow < 30; intRow++){
+      for(int intColumn = 0; intColumn < 30; intColumn++){
+        intX = 603 + (intRow * 10);
+        intY = 303 + (intColumn * 10); 
+
+        if(intColumn % 2 == 0){
+          fill(0, 0, 0);
+          noStroke();
+          rect(intX, intY, 5, 5);
+        }
+        else {
+          fill(255);
+          noStroke();
+          rect(intX, intY, 5, 5);
+          
+        }
+      }  
+    }
   }
 
-  /**
-   * Use the modulus operator and just one 'if' statement to select the color.
-   */
-  public void draw_section4(){
+  // Checkered grid 
+  public void quad4(){
+    int intX = 0;
+    int intY = 0;
 
-  }
+    for(int intRow = 0; intRow < 30; intRow++){
+      for(int intColumn = 0; intColumn < 30; intColumn++){
+        intX = 903 + (intRow * 10);  
+        intY = 303 + (intColumn * 10); 
 
-  /**
-   * Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead
-   */
-  public void draw_section5(){
-
-  }
-
-  public void draw_section6(){
-
-  }
-
-  public void draw_section7(){
-
+        fill(0, 0, 0);
+        noStroke();
+        rect(intX, intY, 5, 5);
+        
+        if(intColumn % 2 != 0 && intRow % 2 == 0){
+          fill(255);
+          noStroke();
+          rect(intX, intY, 5, 5);
+          
+        }
+      }
+    }
   }
   
-  public void draw_section8(){
+  // Bottom right triangle
+  public void quad5(){
+    int intX = 0;
+    int intY = 0;
+    int intCounter = 0;
 
+    for(int intColumn = 30; intColumn > 0; intColumn--){
+      for(int intRow = 30; intRow > intCounter; intRow--){
+        intX = 3 - 10 + intRow * 300 / 30 ;
+        intY = 3 + intColumn * 300 / 30;
+
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+        
+      }
+      intCounter++;
+      
+    }
   }
 
+  // Bottom left triangle
+  public void quad6(){
+    int intX = 0;
+    int intY = 0;
+    int intCounter = 30;
 
+    for(int intColumn = 30; intColumn > 0; intColumn--){
+      for(int intRow = 0; intRow < intCounter; intRow++){
+        intX = 303 + intRow * 300 / 30 ;
+        intY = 3 - 10 + intColumn * 300 / 30;
 
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+        
+      }
+      intCounter--;
+      
+    }
+  }
 
+  // Top left triangle
+  public void quad7(){
+    int intX = 0;
+    int intY = 0;
+    int intCounter = 30;
 
+    
+    for(int intRow = 0; intRow < 30; intRow++){
+      for(int intColumn = 0; intColumn < intCounter; intColumn++){
+        intX = 603 + intRow * 300 / 30 ;
+        intY = 3 + intColumn * 300 / 30;
 
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+        
+      }
+      intCounter--;
+      
+    }
+  }
+
+  // Top right triangle
+  public void quad8(){
+    
+    int intX = 0;
+    int intY = 0;
+    int intCounter = 30;
+
+    for(int intRow = 30; intRow > 0; intRow--){
+      for(int intColumn = 0; intColumn < intCounter; intColumn++){
+        intX = 903 - 10 + intRow * 300 / 30 ;
+        intY = 3 + intColumn * 300 / 30;
+
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+        
+      }
+      intCounter--;
+      
+    }
+    
+  }
 }
